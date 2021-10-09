ef447f97f8c24f6a0f69e9d58ad8a491d6ebf2a5
Kamrul Zaman <kamrul.zaman@CBT.LCL>
2021-10-09 19:16:33 +0600

    first commint

---
+++ auto-deploy/src/test/java/com/kzarif/AutoDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzarif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
