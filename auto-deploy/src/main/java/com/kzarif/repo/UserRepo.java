cef23e43a71218dcaf84f90542e352709b82c001
Kamrul Zaman <kamrul.zaman@CBT.LCL>
2021-10-10 21:42:59 +0600

    some change

---
+++ auto-deploy/src/main/java/com/kzarif/repo/UserRepo.java
@@ -0,0 +1,9 @@
+package com.kzarif.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kzarif.entity.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
