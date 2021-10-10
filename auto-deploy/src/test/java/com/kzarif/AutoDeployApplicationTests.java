b5da803fda1b3bd541c24b9dc59bda5e21476cee
Kamrul Zaman <kamrul.zaman@CBT.LCL>
2021-10-10 22:05:09 +0600

    health check

--- auto-deploy/src/test/java/com/kzarif/AutoDeployApplicationTests.java
+++ auto-deploy/src/test/java/com/kzarif/AutoDeployApplicationTests.java
@@ -1,5 +1,7 @@
 package com.kzarif;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class AutoDeployApplicationTests {
 
 	@Test
 	void contextLoads() {
+		String data = "Data";
+		assertTrue( data.equals("Data"));
 	}
 
 }
